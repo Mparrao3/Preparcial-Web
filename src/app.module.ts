2f1c679260ff185be6439d9f6dfb1ec6b07023c3
Mparrao3 <m.parrao@uniandes.edu.co>
2025-11-21 09:54:48 -0500

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountriesModule } from './countries/countries.module';
 import { TravelPlansModule } from './travel-plans/travel-plans.module';
 import { Country } from './countries/entities/country.entity';
 import { TravelPlan } from './travel-plans/entities/travel-plan.entity';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -20,4 +21,13 @@ import { TravelPlan } from './travel-plans/entities/travel-plan.entity';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        { path: 'countries', method: RequestMethod.ALL },
+        { path: 'travel-plans', method: RequestMethod.ALL },
+      );
+  }
+}
